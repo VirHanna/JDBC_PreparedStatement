2d506678cba4f5f4d4a1ea2e34517c7e472ded7f
VirHanna <annabasha527@gmail.com>
2025-04-12 20:09:30 +0300

    Replaced Statement with PreparedStatement in DatabaseInitService.java

--- src/main/java/goit/jdbc/DatabaseInitService.java
+++ src/main/java/goit/jdbc/DatabaseInitService.java
@@ -5,15 +5,15 @@ import goit.jdbc.exception.DatabaseQueryException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class DatabaseInitService {
     public static void main(String[] args) {
         try {
             String sql = Files.readString(Path.of("sql/init_db.sql"));
             Connection conn = Database.getInstance().getConnection();
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
             System.out.println("Database initialized");
         } catch (Exception e) {
             throw new DatabaseQueryException("Failed to initialize the database", e);
